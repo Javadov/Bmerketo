3e6413115067240c069f1b38316e305880cdf18b
javadov <javadov@hotmail.com>
2023-05-17 12:39:10 +0200

    Any Updates

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -1,4 +1,17 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Carousel Auto-Cycle
+  $(document).ready(function() {
+    $('.carousel').carousel({
+      interval: 6000
+    })
+  });
 
-// Write your JavaScript code.
+var loadMoreButton = document.getElementById("load-more");
+
+// Attach a click event listener to the Load More button
+loadMoreButton.addEventListener("click", function () {
+    // Increment the number of products to display
+    productsToDisplay += 4;
+
+    // Re-render the products with the updated number of products to display
+    // You can use your preferred way of doing this, for example with AJAX or by submitting a form
+});
